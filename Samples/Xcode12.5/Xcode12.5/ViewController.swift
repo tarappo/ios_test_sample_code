df75b21cadde7e1d46a6153960720fab9473b34b
tarappo <tarappo@gmail.com>
2021-05-24 16:48:37 +0900

    add Xcode12.5 sample code

---
+++ Samples/Xcode12.5/Xcode12.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode12.5
+//
+//  Created by thirata on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

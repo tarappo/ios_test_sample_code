5b9dcfaf5b23645808e00af733e1cb5d168cc959
tarappo <tarappo@gmail.com>
2024-08-17 16:03:26 +0900

    テストコードの追加と改良

--- Samples/Xcode13.0/Xcode13.0Tests/Xcode13_0Tests.swift
+++ Samples/Xcode13.0/Xcode13.0Tests/Xcode13_0Tests.swift
@@ -9,15 +9,33 @@ import XCTest
 @testable import Xcode13_0
 
 final class Xcode13_0Tests: XCTestCase {
-
     override func setUpWithError() throws {
     }
-
+    
     override func tearDownWithError() throws {
+    }
+    
+    // 非同期テスト
+    func test_chopVegetables() async throws {
+        let vegetables = try await chopVegetables()
+        XCTAssertEqual(vegetables.count, 2)
     }
 
     // 連続で実行されるようのテストケース
     func testRepeatExample() throws {
         XCTAssert(true)
+    }
+}
+
+extension Xcode13_0Tests {
+    enum Vegetable {
+        case carrot
+        case watermelon
+    }
+    func chopVegetables() async throws -> [Vegetable] {
+        try await Task.sleep(for: .seconds(1.0))
+        let vegetables: [Vegetable] = [.carrot, .watermelon]
+
+        return vegetables
     }
 }

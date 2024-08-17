334e5fe3fbe2826c8d542f9f1b35d4dd54d3b080
tarappo <tarappo@gmail.com>
2024-08-17 17:49:40 +0900

    Xcode15.0用のサンプルコード雛形用意

---
+++ Samples/Xcode15.0/README.md
@@ -0,0 +1,10 @@
+# Xcode 15.0
+ - [Release Note](https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes)
+
+## Testing
+ - （１）xcodebuildに-enumerate-testsオプションを追加
+ - （２）テストの実行結果の改善
+ - （３）UIテストの実行を自動録画
+
+## 参考資料
+ -  https://note.com/tarappo/n/nf8f00b92159c?magazine_key=m8710a4f80fae
---
+++ Samples/Xcode15.0/Xcode15.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode15.0
+//
+//  Created by Toshiyuki Hirata on 2024/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

ab64a24fdbc46d284dd1af4e9d443fd79ee5cd77
tarappo <tarappo@gmail.com>
2024-09-29 14:18:00 +0900

    コメントの追加

--- Samples/Xcode16.0/xcode16Tests/ParameterizedTest.swift
+++ Samples/Xcode16.0/xcode16Tests/ParameterizedTest.swift
@@ -9,13 +9,13 @@
 import Testing
 
 struct ParameterizedTest {
-    
-    // Parameterized Test Sample
+    // Parameterized Test Sample String
     @Test("Parameterized test", arguments: ["sample", "sample2", "sample3"])
     func parameterizedTest(name: String) {
         #expect(name.contains("sample"))
     }
-    
+
+    // Parameterized Test Sample Int
     @Test("Parameterized Test using Int", arguments: [1, 2, 3])
     func parameterizedTestUsingInt(number: Int) {
         #expect(number < 4)
